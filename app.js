3c33860c0a579a314c788d0ed17efe3447725ecc
Aakil Fernandes <aakilfernandes@gmail.com>
2015-08-24 11:57:05 -0400

    batch web3 requests

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ if(!web3.currentProvider)
 
 var lottoAbi = [{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getTickets","outputs":[{"name":"","type":"address[]"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getPot","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"getIsCashed","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getSubpotsCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"calculateWinner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"getRoundIndex","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"blockIndex","type":"uint256"}],"name":"getHashOfBlock","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"getBlocksPerRound","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"getTicketPrice","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getSubpot","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"cash","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"getDecisionBlockNumber","outputs":[{"name":"","type":"uint256"}],"type":"function"}]
 	,Lotto = web3.eth.contract(lottoAbi).at('0xdf415bb7ae2363ecbb6a595f07cbc2fc0fd417d3')
+	,blocksPerRound = 6800
 
 app.get('/', function (req, res) {
 	var filePath = path.join(__dirname, '/views/index.html')
@@ -18,43 +19,92 @@ app.get('/', function (req, res) {
 
 app.get('/api/lotto',function(req, res){
 
-	var roundIndex = req.params.roundIndex
-		,round = {
-			roundIndex:parseInt(Lotto.getRoundIndex())
-			,blocksPerRound:Lotto.getBlocksPerRound()
-			,ticketPrice:Lotto.getTicketPrice()
-			,blockNumber:web3.eth.blockNumber
-		}
+	var batch = web3.createBatch()
+	var lotto = {blocksPerRound:blocksPerRound}
 
-	res.send(round)
+	batch.add(Lotto.getRoundIndex.request(function(error,roundIndex){
+		lotto.roundIndex = parseInt(roundIndex)
+	}))
+	batch.add(Lotto.getTicketPrice.request(function(error,ticketPrice){
+		lotto.ticketPrice = ticketPrice
+	}))
+	batch.add(web3.eth.getBlockNumber.request(function(results,blockNumber){
+		lotto.blockNumber = parseInt(blockNumber)
+		res.send(lotto);
+	}))
+
+	batch.execute()
+
 })
 
 
 app.get('/api/rounds/:roundIndex',function(req, res){
 
+	var batch = web3.createBatch()
+	var round = {subpots:[]}
 	var roundIndex = req.params.roundIndex
-		,round = {
-			pot:Lotto.getPot(roundIndex)
-			,tickets:Lotto.getTickets(roundIndex)
-			,subpot:Lotto.getSubpot(roundIndex)
-			,subpotsCount:Lotto.getSubpotsCount(roundIndex)
-			,subpots:[]
+	var blockNumber
+
+	batch.add(web3.eth.getBlockNumber.request(function(results,blockNumber){
+		blockNumber = parseInt(blockNumber)
+	}))
+	
+
+	batch.add(Lotto.getSubpotsCount.request(roundIndex,function(results,subpotsCount){
+
+		round.subpotsCount = parseInt(subpotsCount)
+
+
+		var batch = web3.createBatch()
+
+		for(var subpotIndex = 0; subpotIndex<round.subpotsCount; subpotIndex++){
+
+			var decisionBlockNumber = (roundIndex*blocksPerRound)+subpotIndex
+			var _subpotIndex = subpotIndex
+
+			round.subpots[subpotIndex]={decisionBlockNumber:decisionBlockNumber}
+
+			if(blockNumber>decisionBlockNumber){
+				round.subpots[subpotIndex].decisionBlockHash=null
+				round.subpots[subpotIndex].winner = null
+				round.subpots[subpotIndex].isCashed = null
+				continue;
+			}
+
+			(function(subpotIndex){
+				batch.add(web3.eth.getBlock.request(decisionBlockNumber,function(error,block){
+					round.subpots[subpotIndex].decisionBlockHash = 1;
+					round.subpots[subpotIndex].decisionBlockHash = block.hash
+				}))
+
+				batch.add(Lotto.calculateWinner.request(roundIndex,subpotIndex,function(error,winner){
+					round.subpots[subpotIndex].winner = winner
+				}))
+
+				batch.add(Lotto.getIsCashed.request(roundIndex,subpotIndex,function(error,isCashed){
+					round.subpots[subpotIndex].isCashed = isCashed
+				}))
+			}(subpotIndex))
+		
 		}
 
-	for(var subpotIndex = 0; subpotIndex<round.subpotsCount; subpotIndex++){
+		batch.add(Lotto.getPot.request(roundIndex,function(error,pot){
+			round.pot = parseInt(pot)
+		}))
+		batch.add(Lotto.getTickets.request(roundIndex,function(error,tickets){
+			round.tickets = tickets
+		}))
+		batch.add(Lotto.getSubpot.request(roundIndex,function(error,subpot){
+			round.subpot = subpot
+			res.send(round);
+		}))
 
-		var decisionBlockNumber = Lotto.getDecisionBlockNumber(roundIndex,subpotIndex)
-			,decisionBlock = web3.eth.getBlock(decisionBlockNumber)
+		batch.execute()
+	
+	}))
 
-		round.subpots.push({
-			decisionBlockNumber:decisionBlockNumber
-			,decisionBlockHash:decisionBlock?decisionBlock.hash:null
-			,winner:decisionBlock?Lotto.calculateWinner(roundIndex,subpotIndex):null
-			,isCashed:Lotto.getIsCashed(roundIndex,subpotIndex)
-		})
-	}
+	batch.execute()
 
-	res.send(round)
 })
 
 app.listen(3000)
