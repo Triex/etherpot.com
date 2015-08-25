bc087259a71d133b8f8dbd80fefc390095956a1f
Aakil Fernandes <aakilfernandes@gmail.com>
2015-08-25 18:03:48 -0400

    cors

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 var express = require('express')
 	,app = express()
 	,web3 = require('web3')
-	,path = require('path');
+	,path = require('path')
+	,cors = require('cors')
 
-
- if(!web3.currentProvider)
-     web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
-
- var LottoAbi = [{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getPot","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"buyer","type":"address"}],"name":"getTicketsCountByBuyer","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"getIsCashed","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"buyer","type":"address"}],"name":"getBuyers","outputs":[{"name":"","type":"address[]"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getSubpotsCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"calculateWinner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"getRoundIndex","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"blockIndex","type":"uint256"}],"name":"getHashOfBlock","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"getBlocksPerRound","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"getTicketPrice","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getSubpot","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"cash","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"getDecisionBlockNumber","outputs":[{"name":"","type":"uint256"}],"type":"function"}];LottoContract = web3.eth.contract(LottoAbi);Lotto = LottoContract.at('0x96268285750b282830797f2c43d6ef941cb78596')
-	,Lotto = web3.eth.contract(LottoAbi).at('0x539f2912831125c9b86451420bc0d37b219587f9')
- 	,blocksPerRound = 6800
+app.use(cors());
 
 app.get('/api/lotto',function(req, res){
 
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	 if(!web3.currentProvider)
+     web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
-	var batch = web3.createBatch()
-	var lotto = {blocksPerRound:blocksPerRound}
+	 var LottoAbi = [{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getPot","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"buyer","type":"address"}],"name":"getTicketsCountByBuyer","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"getIsCashed","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"buyer","type":"address"}],"name":"getBuyers","outputs":[{"name":"","type":"address[]"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getSubpotsCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"calculateWinner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"getRoundIndex","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"blockIndex","type":"uint256"}],"name":"getHashOfBlock","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"getBlocksPerRound","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"getTicketPrice","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"}],"name":"getSubpot","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"cash","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"roundIndex","type":"uint256"},{"name":"subpotIndex","type":"uint256"}],"name":"getDecisionBlockNumber","outputs":[{"name":"","type":"uint256"}],"type":"function"}];LottoContract = web3.eth.contract(LottoAbi);Lotto = LottoContract.at('0x96268285750b282830797f2c43d6ef941cb78596')
+		,Lotto = web3.eth.contract(LottoAbi).at('0x539f2912831125c9b86451420bc0d37b219587f9')
+	 	,blocksPerRound = 6800
+		,batch = web3.createBatch()
+		,lotto = {blocksPerRound:blocksPerRound}
 
 	batch.add(Lotto.getRoundIndex.request(function(error,roundIndex){
 
@@ -39,7 +37,7 @@ app.get('/api/lotto',function(req, res){
 
 		batch.add(Lotto.getBuyers.request(roundIndex,function(error,buyers){
 			lotto.buyers = buyers
-			res.send(lotto)
+			res.json(lotto)
 		}))
 
 		batch.execute()
